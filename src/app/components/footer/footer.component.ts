9505a0666d027999fd39c1fe7ef8035b4549a3d0
vinyard78 <luck.ballaud@wanadoo.fr>
2020-01-16 08:40:49 +0100

    bugfix

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,4 +1,6 @@
-import { Component, Output, TemplateRef, Input } from '@angular/core';
+import { Component, HostListener, Output, TemplateRef, Input, Inject } from '@angular/core';
+import { DOCUMENT } from "@angular/common";
+import { WINDOW } from "./../../services/window.service";
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 import { Globals } from "./../../globals";
@@ -12,9 +14,14 @@ export class FooterComponent {
 	@Input() forceDisplay: boolean;
 	modalRef: BsModalRef;
 
+	screenHeight:number = 0;
+  	screenWidth:number = 0;
+
 	constructor(
 		private modalService: BsModalService,
-		public globals: Globals) {
+		public globals: Globals,
+		@Inject(DOCUMENT) private document: Document,
+    	@Inject(WINDOW) public window) {
 	}
 
 	openModal(legal: TemplateRef<any>) {
@@ -22,6 +29,13 @@ export class FooterComponent {
 	}
 
  	ngOnInit() {
+ 		this.screenHeight = this.window.innerHeight;
+        this.screenWidth = this.window.innerWidth;
 	}
+
+	@HostListener('window:resize', []) onResize() {
+        this.screenHeight = this.window.innerHeight;
+        this.screenWidth = this.window.innerWidth;
+    }
 
 }
